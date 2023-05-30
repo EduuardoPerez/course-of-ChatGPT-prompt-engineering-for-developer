708530b6cbccad4c5572eee4a29f8a08c4dbd5a7
Eduardo Perez <edujpp1@gmail.com>
2023-05-30 07:37:45 -0300

    Apply principle 1, tactic 4: "Few-shot" prompting

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ then simply write \"No steps provided.\"
 \"\"\"{}\"\"\"
 
 """
-response = get_completion(prompt_3.format(text_2))
+response_3 = get_completion(prompt_3.format(text_2))
 print("Completion for Text 2:")
-print(response)
+print(response_3)
 # response
 # Completion for Text 1:
 # Step 1 - Get some water boiling.
@@ -130,3 +130,22 @@ print(response)
 # response
 # Completion for Text 2:
 # No steps provided.
+
+# ** Tactic 4: "Few-shot" prompting
+prompt_4 = """
+Your task is to answer in a consistent style.
+<child>: Teach me about patience.
+
+<grandparent>: The river that carves the deepest \
+valley flows from a modest spring; the \
+grandest symphony originates from a single note; \
+the most intricate tapestry begins with a solitary thread.
+
+<child>: Teach me about resilience.
+"""
+response_4 = get_completion(prompt_4)
+print(response_4)
+# response
+# <grandparent>: Resilience is like a tree that bends with the wind but never breaks. It is the ability to bounce back
+# from adversity and keep moving forward, even when things get tough. Just like a tree that grows stronger with each
+# storm it weathers, resilience is a quality that can be developed and strengthened over time.
