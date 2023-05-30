bc680f60211a7306a1258268fc2f5a0e1dcf0978
Eduardo Perez <edujpp1@gmail.com>
2023-05-30 07:19:23 -0300

    Apply principle 1, tactic 3: Ask the model to check whether conditions are satisfied

--- main.py
+++ main.py
@@ -70,3 +70,63 @@ print(response_2)
 #     {"book_id": 2, "title": "The Last Survivors", "author": "Ethan Stone", "genre": "Science Fiction"},
 #     {"book_id": 3, "title": "The Secret of the Haunted Mansion", "author": "Lila Rose", "genre": "Mystery"},
 # ]
+
+
+# ** Tactic 3: Ask the model to check whether conditions are satisfied
+text_2 = """
+Making a cup of tea is easy! First, you need to get some \
+water boiling. While that's happening, \
+grab a cup and put a tea bag in it. Once the water is \
+hot enough, just pour it over the tea bag. \
+Let it sit for a bit so the tea can steep. After a \
+few minutes, take out the tea bag. If you \
+like, you can add some sugar or milk to taste. \
+And that's it! You've got yourself a delicious \
+cup of tea to enjoy.
+"""
+prompt_3 = """
+You will be provided with text delimited by triple quotes.
+If it contains a sequence of instructions, \
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \
+then simply write \"No steps provided.\"
+
+\"\"\"{}\"\"\"
+
+"""
+response = get_completion(prompt_3.format(text_2))
+print("Completion for Text 2:")
+print(response)
+# response
+# Completion for Text 1:
+# Step 1 - Get some water boiling.
+# Step 2 - Grab a cup and put a tea bag in it.
+# Step 3 - Once the water is hot enough, pour it over the tea bag.
+# Step 4 - Let it sit for a bit so the tea can steep.
+# Step 5 - After a few minutes, take out the tea bag.
+# Step 6 - Add some sugar or milk to taste.
+# Step 7 - Enjoy your delicious cup of tea!
+
+text_3 = """
+The sun is shining brightly today, and the birds are \
+singing. It's a beautiful day to go for a \
+walk in the park. The flowers are blooming, and the \
+trees are swaying gently in the breeze. People \
+are out and about, enjoying the lovely weather. \
+Some are having picnics, while others are playing \
+games or simply relaxing on the grass. It's a \
+perfect day to spend time outdoors and appreciate the \
+beauty of nature.
+"""
+response = get_completion(prompt_3.format(text_3))
+print("Completion for Text 2:")
+print(response)
+# response
+# Completion for Text 2:
+# No steps provided.
